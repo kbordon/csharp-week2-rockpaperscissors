f1460883d8db430a5ed72b8abe7303538d7c2f77
Kimberly Bordon and Rakhee Gandhi <rakhee@>
2017-10-18 16:54:18 -0700

    Add styling.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,11 +13,35 @@ namespace Rock.Controllers
         return View();
       }
 
-      [HttpPost("/winner")]
+      [HttpPost("/players/one")]
+      public ActionResult OnePlayer()
+      {
+        return View();
+      }
+
+      [HttpPost("/players/two")]
+      public ActionResult TwoPlayers()
+      {
+        return View();
+      }
+
+      [HttpPost("/players/result")]
       public ActionResult Winner()
       {
-        Game newGame = new Game(Request.Form["player1"], Request.Form["player2"]);
-        return View("Winner", newGame.SelectWinner());
+        Game.ClearAll();
+        if (Request.Form["player2"] == "computer")
+        {
+          string[] computerChoice = {"rock", "paper", "scissors"};
+          Random rnd = new Random();
+          int computerChoiceIndex = rnd.Next(0,2);
+          Game newGame = new Game(Request.Form["player1"], computerChoice[computerChoiceIndex]);
+          return View("Winner", newGame);
+        }
+        else
+        {
+          Game newGame = new Game(Request.Form["player1"], Request.Form["player2"]);
+          return View("Winner", newGame);
+        }
       }
     }
 }
